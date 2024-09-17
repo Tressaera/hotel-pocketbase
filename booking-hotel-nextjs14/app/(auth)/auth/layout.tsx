9d1f8d569da03ec9fda081bc42d6fd02c488d6bd
= <mattshady1067@gmail.com>
2024-09-18 00:09:00 +0300

    login and registration page created

--- booking-hotel-nextjs14/app/(auth)/auth/layout.tsx
+++ booking-hotel-nextjs14/app/(auth)/auth/layout.tsx
@@ -1,15 +1,44 @@
-import React, { ReactNode } from 'react'
+'use client'
 
+import { Loader2 } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
 
-interface AuthLayoutProps{
-    children:React.ReactNode
+interface AuthLayoutProps {
+  children: React.ReactNode
 }
-const AuthLayout = ({children}:AuthLayoutProps) => {
+const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <div>
-      Logo
-      {children}
-      logo bitiş
+    <div className='h-screen flex justify-center items-center bgone'>
+      <div className='hidden lg:block w-1/2 h-full'>
+        <Image
+          alt='login'
+          src="/auth.jpg"
+          width={1080}
+          height={1920}
+          className='h-full w-full object-cover'
+        />
+
+      </div>
+      <div className='w-full lg:w-1/2 p-10 flex flex-col items-center'>
+        <div className='flex items-center mb-6'>
+          <div className='w-24 py-4 px-3 rounded-xl bg-white'>
+            <Link href="/">
+                <Image
+                  src="/logo.png"
+                  alt='logo'
+                  width={500}
+                  height={500}
+                  className='w-full'
+                />
+            </Link>
+          </div>
+        </div>
+        {children}
+      </div>
+
     </div>
   )
 }
